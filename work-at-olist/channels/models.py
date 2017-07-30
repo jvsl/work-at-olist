a788635b56337642295a2aba8864fd5ff0aa4e50
JV <ccjoaovictor@gmail.com>
2017-07-30 16:54:25 -0300

    added django mptt library - added channel and category models

--- work-at-olist/channels/models.py
+++ work-at-olist/channels/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+import uuid
 
-# Create your models here.
+
+class Channel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=254)
+
+
+class Category(MPTTModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=254)
+    parent = TreeForeignKey('self', null=True, blank=True,
+                            related_name='children', db_index=True)
+    channel = models.ForeignKey('Channel', null=True, blank=True)
